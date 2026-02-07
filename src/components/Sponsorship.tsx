7b18a0a43b940b09c629296cbbd28a41e7541342
michaeloye <michaeloye23@gmail.com>
2026-02-07 17:33:52 +0100

    Update image name

--- src/components/Sponsorship.tsx
+++ src/components/Sponsorship.tsx
@@ -7,7 +7,7 @@ const events = [
     title: "Race Weekend Experience",
     subtitle: "Yas Marina circuit",
     description: "An electrifying large-scale music experience set against the adrenaline-fueled backdrop of the Formula 1 weekend. Positioned near the iconic Yas Marina Circuit, the event brings together globally renowned DJs, world-class stage production, and immersive visual storytelling to create an atmosphere that mirrors the intensity and prestige of the race itself. It merges the worlds of speed, sound, and spectacle - amplifying the energy of Abu Dhabi's most thrilling weekend.",
-    image: "./Check Mate Events_files/ad-race-weekend-experience-2.png",
+    image: "./Check Mate Events_files/yacht-party-during-f1-night-race.png",
     location: "Yas Marina Circuit, Abu Dhabi",
     date: "December 2026",
     audience: "HNW guests, global creatives, luxury partners"
